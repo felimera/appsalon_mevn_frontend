34c9d4574b1e6de897bb41ea2a7b54cabd566d71
andres.mera <feli041089@gmail.com>
2023-09-26 08:55:32 -0500

    Definiendo Rutas y Vistas para Nuevas citas

---
+++ src/views/appointments/AppointmentsLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1>AppointmentsLayout</h1>
+    </div>
+    <main>
+        <RouterView />
+    </main>
+</template>
+
---
+++ src/views/appointments/NewAppointmentsLayout.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <h1>New Appointments</h1>
+
+    <RouterView />
+</template>
+
