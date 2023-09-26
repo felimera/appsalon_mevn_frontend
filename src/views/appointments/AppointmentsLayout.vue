dc4bf88214262e42b7764ada70ab78b1eda65a36
andres.mera <feli041089@gmail.com>
2023-09-26 09:51:14 -0500

    Arreglando la tabulacion

--- src/views/appointments/AppointmentsLayout.vue
+++ src/views/appointments/AppointmentsLayout.vue
@@ -11,15 +11,15 @@ import { RouterLink } from 'vue-router';
                 <p class="text-white text-right">Hola: Usuario</p>
 
                 <button type="button"
-                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">Cerrar
-                    Sesión
+                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">
+                    Cerrar Sesión
                 </button>
             </div>
             <nav class="flex gap-2 items-center justify-end">
                 <button class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg">
                     Mis Citas
                 </button>
-                
+
                 <RouterLink :to="{ name: 'new-appointment' }"
                     class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700">
                     Nueva Cita
