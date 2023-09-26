387a9a6ad025bb3bae49f4931b8a94bbe62bfbc5
andres.mera <feli041089@gmail.com>
2023-09-26 08:09:15 -0500

    Configuracion del Tailwind y limpieza del proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl">AppSalon</h1>
   </main>
 </template>
