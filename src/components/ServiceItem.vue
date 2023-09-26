418de336add1ab5b18da1296b4e5460026454150
andres.mera <feli041089@gmail.com>
2023-09-26 13:56:00 -0500

    Creando el Store de Citas y obteniendo el Servicio al dar Click

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { formatCurrency } from '../helpers';
+import { useAppointmentsStore } from '../stores/appointments';
+
+const appointments = useAppointmentsStore();
 
 defineProps({
     service: {
@@ -10,7 +13,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white">
+    <div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white" @click="appointments.onServiceSelected(service)">
         <p class="text-2xl font-light">{{ service.name }}</p>
         <p class="text-4xl font-black text-blue-600">{{ formatCurrency(service.price) }}</p>
     </div>
