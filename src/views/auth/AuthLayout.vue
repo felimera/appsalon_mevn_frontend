d7153eaedf937b3af720a72b0baa4a8f2fafca5f
andres.mera <feli041089@gmail.com>
2023-09-28 13:42:56 -0500

    Definiendo Rutas y Layout de Registro de Usuarios y Autenticación.

---
+++ src/views/auth/AuthLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+    <div>
+<h1>Desde Auth Layout</h1>
+    </div>
+
+    <RouterView/>
+</template>
+
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div>
+<h1>Desde RegisterView</h1>
+    </div>
+</template>
+
