d0c0d6e23c8e19b77dc6394b76dd3e533ebb223c
andres.mera <feli041089@gmail.com>
2023-09-28 14:24:05 -0500

    Primeros pasos Registro e Instalando FormKit

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,8 +1,12 @@
 <script setup></script>
 
 <template>
-    <div>
-<h1>Desde RegisterView</h1>
-    </div>
+    <h1 class="text-6xl font-extrabold text-white text-center mt-10">Crea una Cuenta</h1>
+    <p class="text-2xl text-white text-center my-5">Crea un cuenta en AppSalón</p>
+
+    <Formkit type="form">
+        <FormKit type="text" />
+
+    </Formkit>
 </template>
 
