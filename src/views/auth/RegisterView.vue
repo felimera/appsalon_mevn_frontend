087e4a8441541ff5f40934b343619207acb4dde6
andres.mera <feli041089@gmail.com>
2023-09-29 08:08:11 -0500

    Solucion al error del FormKit

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -4,8 +4,8 @@
     <h1 class="text-6xl font-extrabold text-white text-center mt-10">Crea una Cuenta</h1>
     <p class="text-2xl text-white text-center my-5">Crea un cuenta en AppSalón</p>
 
-    <Formkit type="form" :actions="false" incomplete-message="No se pudo enviar revisa las notificaciones">
-        <FormKit type="text" label="Nombre" name="name" placeholder="Tu Nombre" validation="required|length:3"
+    <FormKit type="form" :actions="false" incomplete-message="No se pudo enviar revisa las notificaciones">
+         <FormKit type="text" label="Nombre" name="name" placeholder="Tu Nombre" validation="required|length:3"
             :validation-messages="{
                 required: 'El Nombre es obligatorio',
                 length: 'El nombre muy corto'
@@ -13,6 +13,6 @@
 
         <FormKit type="submit">Crear Cuenta</FormKit>
 
-    </Formkit>
+    </FormKit>
 </template>
 
