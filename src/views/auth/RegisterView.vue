4c94c76b382baeea4efc6ca9406b9a39d1e70b55
andres.mera <feli041089@gmail.com>
2023-09-29 08:19:17 -0500

    Añadiendo el Resto de los Campos y su validación

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -5,10 +5,28 @@
     <p class="text-2xl text-white text-center my-5">Crea un cuenta en AppSalón</p>
 
     <FormKit type="form" :actions="false" incomplete-message="No se pudo enviar revisa las notificaciones">
-         <FormKit type="text" label="Nombre" name="name" placeholder="Tu Nombre" validation="required|length:3"
+        <FormKit type="text" label="Nombre" name="name" placeholder="Tu Nombre" validation="required|length:3"
             :validation-messages="{
                 required: 'El Nombre es obligatorio',
                 length: 'El nombre muy corto'
+            }" />
+
+        <FormKit type="email" label="Email" name="email" placeholder="Email de Registro" validation="required|email"
+            :validation-messages="{
+                required: 'El Nombre es obligatorio',
+                email: 'Email no válido'
+            }" />
+
+        <FormKit type="password" label="Password" name="password" placeholder="Password de Usuario - Min 8 caracteres"
+            validation="required|length:8" :validation-messages="{
+                required: 'El Nombre es obligatorio',
+                length: 'El password debe tener al menos 8 caracteres'
+            }" />
+
+        <FormKit type="password" label="Repetir Password" name="password_confirm" placeholder="Repite el Password"
+            validation="required|confirm" :validation-messages="{
+                required: 'El Nombre es obligatorio',
+                confirm: 'Los passwords no son iguales'
             }" />
 
         <FormKit type="submit">Crear Cuenta</FormKit>
