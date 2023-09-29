d7fcd838ed2f9569f3668632a5b3ad13663a279b
andres.mera <feli041089@gmail.com>
2023-09-29 09:57:05 -0500

    Leer el Token de la URL y confirmar al Usuario

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,8 +1,24 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import AuthAPI from '../../api/AuthAPI';
+
+const route = useRoute();
+const { token } = route.params;
+
+onMounted(async () => {
+    try {
+        const { data } = await AuthAPI.verifyAccount(token);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+</script>
 
 <template>
     <div>
-<h1>Desde Confirm Account</h1>
+        <h1>Desde Confirm Account</h1>
     </div>
 </template>
 
