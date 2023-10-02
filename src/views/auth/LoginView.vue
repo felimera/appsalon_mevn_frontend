9d5bd6257e4c63e38b64ccc97ef10c85e629c16b
andres.mera <feli041089@gmail.com>
2023-10-02 07:53:53 -0500

    Creando el formulario de login

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,8 +1,27 @@
-<script setup></script>
+<script setup>
+const handleSubmit = async (data) => {
+    console.log(data);
+}
+</script>
 
 <template>
-    <div>
-<h1>Desde Login</h1>
-    </div>
+    <h1 class="text-6xl font-extrabold text-white text-center mt-10">Iniciar Sesión</h1>
+    <p class="text-2xl text-white text-center my-5">Si tienes una cuenta, inicia sesión</p>
+
+    <FormKit id="loginForm" type="form" :actions="false" incomplete-message="No se pudo enviar revisa las notificaciones"
+        @submit="handleSubmit">
+
+        <FormKit type="email" label="Email" name="email" placeholder="Email de Usuario" validation="required|email"
+            :validation-messages="{
+                required: 'El Nombre es obligatorio',
+                email: 'Email no válido'
+            }" />
+
+        <FormKit type="password" label="Password" name="password" placeholder="Password de Usuario" validation="required"
+            :validation-messages="{ required: 'El Nombre es obligatorio' }" />
+
+        <FormKit type="submit">Iniciar Sesión</FormKit>
+
+    </FormKit>
 </template>
 
