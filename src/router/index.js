e3f307edb8ae76d803d26faeeff515635aef1351
andres.mera <feli041089@gmail.com>
2023-10-02 09:36:06 -0500

    Protegiendo Rutas con el Guard de Navegación

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ const router = createRouter({
       path: "/reservaciones",
       name: "appointments",
       component: AppointmentsLayout,
+      meta:{requiresAuth:true},
       children: [
         {
           path: '',
@@ -64,5 +65,21 @@ const router = createRouter({
     },
   ],
 });
+
+router.beforeEach(async (to,from,next)=>{
+const requiresAuth=to.matched.some(url=>url.meta.requiresAuth);
+if(requiresAuth)
+{
+  try {
+    
+  } catch (error) {
+    console.log(error)
+  }
+}else
+{
+  next();
+}
+
+})
 
 export default router;
