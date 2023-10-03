4053a746ac8580bc5e9562076eda0c5bd99fbcec
andres.mera <feli041089@gmail.com>
2023-10-03 14:52:06 -0500

    Routing en Vue.js para edición de Citas

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { displayDate } from '../helpers/date';
 import { formatCurrency } from '../helpers';
+import { RouterLink } from 'vue-router';
 defineProps({
     appointment: {
         type: Object
@@ -23,9 +24,10 @@ defineProps({
             Total a pagar : <span class="text-blue-600">{{ formatCurrency(appointment.totalAmount) }}</span>
         </p>
         <div class="flex gap-2 items-center">
-            <button class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">
+            <RouterLink :to="{ name: 'edit-appointment', params: { id: appointment._id } }"
+                class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">
                 Editar Cita
-            </button>
+            </RouterLink>
             <button class="bg-red-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">
                 Cancalar Cita
             </button>
