cef72c0b1968ea3e729f1359a751ccb28977154a
andres.mera <feli041089@gmail.com>
2023-10-03 14:35:10 -0500

    Mostrando detalles de la cita Part 02

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { displayDate } from '../helpers/date';
+import { formatCurrency } from '../helpers';
 defineProps({
     appointment: {
         type: Object
@@ -13,6 +14,22 @@ defineProps({
             Fecha : <span class="font-light">{{ displayDate(appointment.date) }}</span>
             Hora : <span class="font-light">{{ appointment.time }}</span>
         </p>
+        <p class="text-lg font-black">Servicios Solicitados en la cita</p>
+        <div v-for="service in appointment.services">
+            <p>{{ service.name }}</p>
+            <p class="text-2xl font-black text-blue-500">{{ formatCurrency(service.price) }}</p>
+        </div>
+        <p class="text-2xl font-black text-right">
+            Total a pagar : <span class="text-blue-600">{{ formatCurrency(appointment.totalAmount) }}</span>
+        </p>
+        <div class="flex gap-2 items-center">
+            <button class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">
+                Editar Cita
+            </button>
+            <button class="bg-red-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">
+                Cancalar Cita
+            </button>
+        </div>
     </div>
 </template>
 
