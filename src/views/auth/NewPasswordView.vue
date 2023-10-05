c5aeb447fe1f130e4a51ed07017845221d4023d9
andres.mera <feli041089@gmail.com>
2023-10-05 08:48:42 -0500

    Validando el Token en Vue.js

--- src/views/auth/NewPasswordView.vue
+++ src/views/auth/NewPasswordView.vue
@@ -1,22 +1,45 @@
 <script setup>
+import { onMounted, inject, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import AuthAPI from '../../api/AuthAPI';
+
+const toast = inject('toast');
+const route = useRoute();
+const { token } = route.params;
+
+const validToken = ref(false);
+
+onMounted(async () => {
+    try {
+        const { data } = await AuthAPI.verifyPasswordResetToken(token);
+        toast.open({ message: data.msg, type: 'success' });
+        validToken.value = true;
+    } catch (error) {
+        toast.open({ message: error.response.data.msg, type: 'error' });
+    }
+});
+
 const handleSubmit = () => { }
 </script>
 
 <template>
-    <h1 class="text-6xl font-extrabold text-white text-center mt-10">Nuevo Password</h1>
-    <p class="text-2xl text-white text-center my-5">Coloca tu nuevo Password</p>
+    <div v-if="validToken">
+        <h1 class="text-6xl font-extrabold text-white text-center mt-10">Nuevo Password</h1>
+        <p class="text-2xl text-white text-center my-5">Coloca tu nuevo Password</p>
 
-    <FormKit id="newPasswordForm" type="form" :actions="false"
-        incomplete-message="No se pudo enviar revisa las notificaciones" @submit="handleSubmit">
+        <FormKit id="newPasswordForm" type="form" :actions="false"
+            incomplete-message="No se pudo enviar revisa las notificaciones" @submit="handleSubmit">
 
-        <FormKit type="password" label="Password" name="password" placeholder="Password de Usuario - Min 8 caracteres"
-            validation="required|length:8" :validation-messages="{
-                required: 'El Password es obligatorio',
-                length: 'El password debe tener al menos 8 caracteres'
-            }" />
+            <FormKit type="password" label="Password" name="password" placeholder="Password de Usuario - Min 8 caracteres"
+                validation="required|length:8" :validation-messages="{
+                    required: 'El Password es obligatorio',
+                    length: 'El password debe tener al menos 8 caracteres'
+                }" />
 
-        <FormKit type="submit">Guardar Password</FormKit>
+            <FormKit type="submit">Guardar Password</FormKit>
 
-    </FormKit>
+        </FormKit>
+    </div>
+    <p v-else class="text-center text-2xl font-black text-white">Token no Válido</p>
 </template>
 
